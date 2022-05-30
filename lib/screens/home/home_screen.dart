bbd15e3f797284974f2045d8fa68d21cac17b054
Milad Khalighi <milad.kh4lighi@gmail.com>
2022-05-30 00:44:45 -0700

    change background gradient color code and make ui scrollable

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -23,76 +23,90 @@ class _HomeScreenState extends State<HomeScreen> {
     var size = MediaQuery.of(context).size;
     var bodyMagin = size.width * 0.05;
 
-    return Container(
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
-          begin: Alignment.topCenter,
-          end: Alignment.bottomCenter,
-            colors: GradientColors.bgHomeGradient)
-      ),
+    return SafeArea(
       child: Scaffold(
-        backgroundColor: const Color(0xFFEDEDED).withOpacity(0.5),
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-             SizedBox(
-               //color: Colors.red,
-               height: 64,
-               child: Row(
-                 crossAxisAlignment: CrossAxisAlignment.center,
-                 children: [
-                   SizedBox(width: bodyMagin,),
-                   ActionButton(icon: const Icon(Icons.ac_unit_rounded,color: Colors.black,), onTap: (){}),
-                   const Spacer(),
-                   ActionButton(icon: const Icon(Icons.ac_unit_rounded,color: Colors.black,), onTap: (){}),
-                   SizedBox(width: bodyMagin,),
-                   ActionButton(icon: const Icon(Icons.ac_unit_rounded,color: Colors.black,), onTap: (){}),
-                   SizedBox(width: bodyMagin,),
-                 ],
-               ),
-             ),
-            Padding(
-              padding: EdgeInsets.only(left: bodyMagin),
-              child: Text('PUPPY',style: GoogleFonts.bebasNeue(fontSize: 40,color: Colors.black),),
-            ),
-            Padding(
-              padding: EdgeInsets.only(left: bodyMagin),
-              child: Text('Choose Your Dress...!',style: GoogleFonts.poppins(fontSize: 20,color: Colors.black),),
-            ),
-            const SizedBox(height: 32,),
-            SizedBox(
-              height: size.height * 0.07,
-              child: ListView.builder(itemBuilder: (context,index)=>
-                  Padding(
-                    padding: EdgeInsets.fromLTRB(index==0 ? bodyMagin : 16, 0, index==dressList.length ? bodyMagin : 0, 0),
-                    child: DressItem(selected: _dressItemIndexSelected==index ? true : false,onPressed: (){
-                      setState((){
-                        _dressItemIndexSelected = index;
-                      });
-                    }, name: dressList[index].name,),
+        backgroundColor: const Color(0xFFEDEDED),
+        body: Container(
+          decoration: BoxDecoration(
+              gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: GradientColors.bgHomeGradient)
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 32),
+                child: SizedBox(
+                  height: 32,
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      SizedBox(width: bodyMagin,),
+                      ActionButton(icon: const Icon(Icons.ac_unit_rounded,color: Colors.black,), onTap: (){}),
+                      const Spacer(),
+                      ActionButton(icon: const Icon(Icons.ac_unit_rounded,color: Colors.black,), onTap: (){}),
+                      SizedBox(width: bodyMagin,),
+                      ActionButton(icon: const Icon(Icons.ac_unit_rounded,color: Colors.black,), onTap: (){}),
+                      SizedBox(width: bodyMagin,),
+                    ],
                   ),
-                itemCount: dressList.length,
-                scrollDirection: Axis.horizontal,
-                shrinkWrap: true,
-                physics: const BouncingScrollPhysics(),
+                ),
               ),
-            ),
-            const SizedBox(height: 8,),
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: bodyMagin),
-              child: BodyCard(height: size.height/1.9),
-            ),
-            const SizedBox(height: 16,),
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: bodyMagin),
-              child: Text('Explore some more products',style: GoogleFonts.poppins(fontSize: 16,fontWeight: FontWeight.w500,color: Colors.black),),
-            ),
-            Padding(
-              padding: EdgeInsets.only(left: bodyMagin),
-              child: PuppyProductCard(size: size),
-            )
+              Expanded(
+                child: SingleChildScrollView(
+                  physics: const BouncingScrollPhysics(),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.only(left: bodyMagin),
+                        child: Text('PUPPY',style: GoogleFonts.bebasNeue(fontSize: 40,color: Colors.black),),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(left: bodyMagin),
+                        child: Text('Choose Your Dress...!',style: GoogleFonts.poppins(fontSize: 20,color: Colors.black),),
+                      ),
+                      const SizedBox(height: 32,),
+                      SizedBox(
+                        height: size.height * 0.07,
+                        child: ListView.builder(itemBuilder: (context,index)=>
+                            Padding(
+                              padding: EdgeInsets.fromLTRB(index==0 ? bodyMagin : 16, 0, index==dressList.length ? bodyMagin : 0, 0),
+                              child: DressItem(selected: _dressItemIndexSelected==index ? true : false,onPressed: (){
+                                setState((){
+                                  _dressItemIndexSelected = index;
+                                });
+                              }, name: dressList[index].name,),
+                            ),
+                          itemCount: dressList.length,
+                          scrollDirection: Axis.horizontal,
+                          shrinkWrap: true,
+                          physics: const BouncingScrollPhysics(),
+                        ),
+                      ),
+                      const SizedBox(height: 8,),
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: bodyMagin),
+                        child: BodyCard(height: size.height/1.9),
+                      ),
+                      const SizedBox(height: 16,),
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: bodyMagin),
+                        child: Text('Explore some more products',style: GoogleFonts.poppins(fontSize: 16,fontWeight: FontWeight.w500,color: Colors.black),),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(left: bodyMagin),
+                        child: PuppyProductCard(size: size),
+                      )
+                    ],
+                  ),
+                ),
+              )
 
-          ],
+            ],
+          ),
         ),
       ),
     );
