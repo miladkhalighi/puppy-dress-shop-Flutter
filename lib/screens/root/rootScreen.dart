911f73c1ec4e40beb256cdbba0c31b71051d700a
Milad Khalighi <milad.kh4lighi@gmail.com>
2022-05-31 06:09:27 -0700

    add some demo screens for better ui

--- lib/screens/root/rootScreen.dart
+++ lib/screens/root/rootScreen.dart
@@ -24,17 +24,61 @@ class _RootScreenState extends State<RootScreen> {
     _controller = PersistentTabController(initialIndex: _bottomNavIndexSelected);
   }
 
-  List<Widget> screens = const [
-    HomeScreen(),
-    Center(
-      child: Text('screen 2'),
+  List<Widget> screens =  [
+    const HomeScreen(),
+    ////////////////////////////////// todo : replace with other screens
+    Container(
+      decoration: BoxDecoration(
+          gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: GradientColors.bgHomeGradient)
+      ),
+      child: Center(
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(11),
+          child: Container(
+              padding: const EdgeInsets.all(32),
+              color: Colors.orange.withOpacity(0.3),
+              child: const Text('Not implement yet',style: TextStyle(fontSize: 20,color: Colors.orange),)),
+        ),
+      ),
     ),
-    Center(
-      child: Text('screen 3'),
+    Container(
+      decoration: BoxDecoration(
+          gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: GradientColors.bgHomeGradient)
+      ),
+      child: Center(
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(11),
+          child: Container(
+              padding: const EdgeInsets.all(32),
+              color: Colors.pink.withOpacity(0.3),
+              child: const Text('Not implement yet',style: TextStyle(fontSize: 20,color: Colors.pink),)),
+        ),
+      ),
     ),
-    Center(
-      child: Text('screen 4'),
+    Container(
+      decoration: BoxDecoration(
+          gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: GradientColors.bgHomeGradient)
+      ),
+      child: Center(
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(11),
+          child: Container(
+              padding: const EdgeInsets.all(32),
+              color: Colors.deepPurple.withOpacity(0.3),
+              child: const Text('Not implement yet',style: TextStyle(fontSize: 20,color: Colors.deepPurple),)),
+        ),
+      ),
     ),
+    ////////////////////////////////////
   ];
 
   List<PersistentBottomNavBarItem> _navBarsItems() {
@@ -77,24 +121,26 @@ class _RootScreenState extends State<RootScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return PersistentTabView(
-        context,
-        screens: screens,
-        decoration: NavBarDecoration(
-          colorBehindNavBar: SolidColors.scaffoldBgColor,
-          gradient: LinearGradient(
-            begin: Alignment.topLeft,
-            end: Alignment.bottomRight,
-              colors: GradientColors.bottomNavGradient
-          )
-        ),
-        items: _navBarsItems(),
-        confineInSafeArea: true,
-      navBarHeight: 64,
-      navBarStyle: NavBarStyle.style10,
-      onItemSelected: (index){
-          setState(()=> _bottomNavIndexSelected = index);
-      },
+    return SafeArea(
+      child: PersistentTabView(
+          context,
+          screens: screens,
+          decoration: NavBarDecoration(
+            colorBehindNavBar: SolidColors.scaffoldBgColor,
+            gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+                colors: GradientColors.bottomNavGradient
+            )
+          ),
+          items: _navBarsItems(),
+          confineInSafeArea: true,
+        navBarHeight: 64,
+        navBarStyle: NavBarStyle.style10,
+        onItemSelected: (index){
+            setState(()=> _bottomNavIndexSelected = index);
+        },
+      ),
     );
   }
 }
